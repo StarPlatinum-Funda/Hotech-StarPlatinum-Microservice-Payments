80f369488e9fe0b025b08cd75efa7c684327ad6e
Pedro Guia <u202212010@upc.edu.pe>
2025-06-09 14:45:36 -0500

    chore: initial commit.

---
+++ src/main/java/com/github/hotechbackend/HotechStarPlatinumMicroservicePaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotechStarPlatinumMicroservicePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotechStarPlatinumMicroservicePaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hotechbackend/HotechStarPlatinumMicroservicePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotechStarPlatinumMicroservicePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
