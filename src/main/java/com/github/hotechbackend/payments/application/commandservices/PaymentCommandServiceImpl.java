eef41698f904ce01230a1f5386f78d7db6e90ff5
Pedro Guia <u202212010@upc.edu.pe>
2025-06-18 09:11:49 -0500

    feat(payment): add update and delete command services implementation.

--- src/main/java/com/github/hotechbackend/payments/application/commandservices/PaymentCommandServiceImpl.java
+++ src/main/java/com/github/hotechbackend/payments/application/commandservices/PaymentCommandServiceImpl.java
@@ -2,6 +2,8 @@ package com.github.hotechbackend.payments.application.commandservices;
 
 import com.github.hotechbackend.payments.domain.model.aggregates.Payment;
 import com.github.hotechbackend.payments.domain.model.commands.CreatePaymentCommand;
+import com.github.hotechbackend.payments.domain.model.commands.DeletePaymentCommand;
+import com.github.hotechbackend.payments.domain.model.commands.UpdatePaymentCommand;
 import com.github.hotechbackend.payments.domain.model.valueobjects.PaymentMethod;
 import com.github.hotechbackend.payments.domain.services.PaymentCommandService;
 import com.github.hotechbackend.payments.infrastructure.persistence.jpa.repositories.PaymentRepository;
@@ -23,5 +25,31 @@ public class PaymentCommandServiceImpl implements PaymentCommandService {
         var payment = new Payment(command);
         paymentRepository.save(payment);
         return Optional.of(payment);
+    }
+
+    @Override
+    public Optional<Payment> handle(UpdatePaymentCommand command) {
+        var result = paymentRepository.findById(command.id());
+        if (result.isEmpty()) throw new IllegalArgumentException("Payment does not exist");
+        var paymentToUpdate = result.get();
+        try {
+            var updatedPayment = paymentRepository.save(paymentToUpdate.updateInformation(command.userId(), command.amount(), command.currency(),
+                    command.paymentMethod(), command.status(), command.payment_date()));
+            return Optional.of(updatedPayment);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while updating Payment: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void handle(DeletePaymentCommand command) {
+        if (!paymentRepository.existsById(command.id())) {
+            throw new IllegalArgumentException("Payment does not exist");
+        }
+        try {
+            paymentRepository.deleteById(command.id());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while deleting payment: " + e.getMessage());
+        }
     }
 }
