560085bbf560ce2d5a43bbbf3589c94d782c3f5e
Pedro Guia <u202212010@upc.edu.pe>
2025-06-18 08:38:54 -0500

    rename file.

--- src/main/java/com/github/hotechbackend/payments/interfaces/rest/transform/CreatePaymentCommandFromResourceAssembler.java
+++ src/main/java/com/github/hotechbackend/payments/interfaces/rest/transform/CreatePaymentCommandFromResourceAssembler.java
@@ -3,7 +3,7 @@ package com.github.hotechbackend.payments.interfaces.rest.transform;
 import com.github.hotechbackend.payments.domain.model.commands.CreatePaymentCommand;
 import com.github.hotechbackend.payments.interfaces.rest.resources.CreatePaymentResource;
 
-public class CreateProfileCommandFromResourceAssembler {
+public class CreatePaymentCommandFromResourceAssembler {
     public static CreatePaymentCommand toCommandFromResource(CreatePaymentResource resource) {
         return new CreatePaymentCommand(resource.id(), resource.userId(), resource.amount(), resource.currency(),
                 resource.paymentMethod(), resource.status(), resource.payment_date());
