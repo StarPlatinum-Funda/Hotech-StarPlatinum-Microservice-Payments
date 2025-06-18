ac078dee332d24aa0ce53af087c9922c143f3874
Pedro Guia <u202212010@upc.edu.pe>
2025-06-18 09:12:01 -0500

    feat(payment): add update payment resource.

---
+++ src/main/java/com/github/hotechbackend/payments/interfaces/rest/resources/UpdatePaymentResource.java
@@ -0,0 +1,22 @@
+package com.github.hotechbackend.payments.interfaces.rest.resources;
+
+import java.util.Date;
+
+public record UpdatePaymentResource(Long userId, float amount, float currency,
+                                    String paymentMethod, String status, Date payment_date) {
+    public UpdatePaymentResource {
+        if (userId == null || userId <= 0) {
+            throw new IllegalArgumentException("User Id must be greater than 0");
+        } else if (amount == 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        } else if (currency == 0) {
+            throw new IllegalArgumentException("Currency must be greater than 0");
+        } else if (paymentMethod == null || paymentMethod.isBlank()) {
+            throw new IllegalArgumentException("Payment Method cannot be null or blank");
+        } else if (status == null || status.isBlank()) {
+            throw new IllegalArgumentException("Status cannot be null or blank");
+        } else if (payment_date == null) {
+            throw new IllegalArgumentException("Payment Date cannot be null or blank");
+        }
+    }
+}
