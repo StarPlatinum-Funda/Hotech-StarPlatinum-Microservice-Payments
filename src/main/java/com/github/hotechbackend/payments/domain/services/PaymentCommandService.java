16dee525567e28451a237ccdb022c4b273779b8e
Pedro Guia <u202212010@upc.edu.pe>
2025-06-18 09:11:39 -0500

    feat(payment): add update and delete command services.

--- src/main/java/com/github/hotechbackend/payments/domain/services/PaymentCommandService.java
+++ src/main/java/com/github/hotechbackend/payments/domain/services/PaymentCommandService.java
@@ -2,6 +2,8 @@ package com.github.hotechbackend.payments.domain.services;
 
 import com.github.hotechbackend.payments.domain.model.aggregates.Payment;
 import com.github.hotechbackend.payments.domain.model.commands.CreatePaymentCommand;
+import com.github.hotechbackend.payments.domain.model.commands.DeletePaymentCommand;
+import com.github.hotechbackend.payments.domain.model.commands.UpdatePaymentCommand;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Service
 public interface PaymentCommandService {
     Optional<Payment> handle(CreatePaymentCommand command);
+    Optional<Payment> handle(UpdatePaymentCommand command);
+    void handle(DeletePaymentCommand command);
 }
