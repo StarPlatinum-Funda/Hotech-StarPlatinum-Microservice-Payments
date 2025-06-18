fbe3cd765ce03d9f42c0b6b7d4793b85e614542f
Pedro Guia <u202212010@upc.edu.pe>
2025-06-18 08:39:42 -0500

    fix payment command and query services.

--- src/main/java/com/github/hotechbackend/payments/domain/services/PaymentCommandService.java
+++ src/main/java/com/github/hotechbackend/payments/domain/services/PaymentCommandService.java
@@ -2,9 +2,11 @@ package com.github.hotechbackend.payments.domain.services;
 
 import com.github.hotechbackend.payments.domain.model.aggregates.Payment;
 import com.github.hotechbackend.payments.domain.model.commands.CreatePaymentCommand;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface PaymentCommandService {
     Optional<Payment> handle(CreatePaymentCommand command);
 }
--- src/main/java/com/github/hotechbackend/payments/domain/services/PaymentQueryService.java
+++ src/main/java/com/github/hotechbackend/payments/domain/services/PaymentQueryService.java
@@ -3,10 +3,12 @@ package com.github.hotechbackend.payments.domain.services;
 import com.github.hotechbackend.payments.domain.model.aggregates.Payment;
 import com.github.hotechbackend.payments.domain.model.queries.GetAllPaymentsQuery;
 import com.github.hotechbackend.payments.domain.model.queries.GetPaymentByIdQuery;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface PaymentQueryService {
     Optional<Payment> handle(GetPaymentByIdQuery query);
     List<Payment> handle(GetAllPaymentsQuery query);
